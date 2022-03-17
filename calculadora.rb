4f17d6ecfd3773408db541193ab0315140c345f9
brenoalex <brenoalex91@gmail.com>
2022-03-17 12:24:08 -0300

    código da calculadora revisado

--- calculadora.rb
+++ calculadora.rb
@@ -1,19 +1,18 @@
 def soma(primeiro_numero, segundo_numero)
-    result = primeiro_numero + segundo_numero
+    primeiro_numero + segundo_numero
 end
 
 def subtracao(primeiro_numero, segundo_numero)
-    result = primeiro_numero - segundo_numero
+    primeiro_numero - segundo_numero
 end
 
 def multiplicacao(primeiro_numero,segundo_numero)
-    result = primeiro_numero*segundo_numero
+    primeiro_numero*segundo_numero
 end
 
-def divisao(primeiro_numero,segundo_numero)
-    if segundo_numero != 0
-        return primeiro_numero/segundo_numero
-    else
-        return 'Opa! Zero como divisor'
-    end
-end
+def divisao (primeiro_numero,segundo_numero) 
+    if segundo_numero == 0 
+        return 'Opa! Zero como divisor' 
+    end 
+    primeiro_numero / segundo_numero 
+end 
